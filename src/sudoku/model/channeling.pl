ec5c9646bf32da97818afecee126650e673b3d64
Bruno Vandekerkhove <Buno@Brunos-MacBook-Air.local>
2019-04-27 15:32:34 +0200

    Channeling Model
    2 variants ;
     - one with all models connected
     - one without channeling between dual models

--- src/sudoku/model/channeling.pl
+++ src/sudoku/model/channeling.pl
@@ -10,7 +10,7 @@
 % @author   Michaël Dooreman & Bruno Vandekerkhove
 % @version  1.0
 
-variant(extended) :- true. % Set to true/fail if you want to use channeling between dual models
+variant(extended) :- fail. % Set to true/fail if you want to use channeling between dual models
 
 % Set up the model for the given puzzle.
 %
@@ -21,7 +21,7 @@ setup_model(channeling, Puzzle, N, K, flatten(Puzzle)) :-
     % This time the domain constraints are in the constraint generation
     %   function because it's easier to write it that way.
     list_2d_to_array(Puzzle, PuzzleArray),
-    generate_constraints_channeling(Variables, PuzzleArray, N, K).
+    generate_constraints_channeling(PuzzleArray, N, K).
 
 % Generate the constraints for the given puzzle.
 %   This includes constraints on domains.
@@ -29,19 +29,30 @@ setup_model(channeling, Puzzle, N, K, flatten(Puzzle)) :-
 % @param Puzzle The puzzle array to generate constraints for.
 % @param N      The dimension of the puzzle.
 % @param K      The dimension of blocks.
-generate_constraints_channeling(Variables, Puzzle, N, K) :-
+generate_constraints_channeling(Puzzle, N, K) :-
     % Declare variables and generate domain constraints
     dim(Dual1, [N,N]), Dual1 :: 1..N,
     dim(Dual2, [N,N]), Dual2 :: 1..N,
     dim(Dual3, [N,N]), Dual3 :: 1..N,
     Puzzle :: 1..N,
     (multifor([Row,Column,Value], 1, N), param(Puzzle, Dual1, Dual2, Dual3, K) do
-        #=(Puzzle[Row,Column], Value, Bool),
-        #=(Dual1[Row,Value], Column, Bool),
-        #=(Dual2[Column,Value], Row, Bool),
-        block(K, Row, Column, Block),
-        position(K, Row, Column, Position),
-        #=(Dual3[Block,Value], Position, Bool)
+        (variant(extended) ->
+            #=(Puzzle[Row,Column], Value, Bool),
+            #=(Dual1[Row,Value], Column, Bool),
+            #=(Dual2[Column,Value], Row, Bool),
+            block(K, Row, Column, Block),
+            position(K, Row, Column, Position),
+            #=(Dual3[Block,Value], Position, Bool)
+        ; % No channeling between dual models
+            #=(Puzzle[Row,Column], Value, Bool1),
+            #=(Dual1[Row,Value], Column, Bool1),
+            #=(Puzzle[Row,Column], Value, Bool2),
+            #=(Dual2[Column,Value], Row, Bool2),
+            block(K, Row, Column, Block),
+            position(K, Row, Column, Position),
+            #=(Puzzle[Row,Column], Value, Bool3),
+            #=(Dual3[Block,Value], Position, Bool3)
+        )
     ).
 
 % Transform the assignments to the decision variables to a solved puzzle.
