3a00eda96b0b69c24c85119c5ca4bd33ce821601
Unknown <michael.dooreman@gmail.com>
2019-05-26 16:10:44 +0200

    Corrected syntax

--- src/hashiwokakero/chr/model.pl
+++ src/hashiwokakero/chr/model.pl
@@ -36,19 +36,19 @@ cell(I,J) ==> I == 1 | north(I,J,0).
 cell(I,J) ==> size(I,_) | south(I,J,0).
 cell(I,J) ==> J == 1 | west(I,J,0).
 cell(I,J) ==> size(_,J) | east(I,J,0).
-cell(I,J), north(I,J,N) ==> I > 1 | N >= 0, N <= 2.
-cell(I,J), south(I,J,S), size(Imax,_) ==> I < Imax | S >= 0, S <= 2.
-cell(I,J), east(I,J,E), size(_,Jmax) ==> J < Jmax | E >= 0, E <= 2.
-cell(I,J), west(I,J,W) ==> J > 1 | W >= 0, W <= 2. 
+cell(I,J), north(I,J,N) ==> I > 1 | N >= 0, N =< 2.
+cell(I,J), south(I,J,S), size(Imax,_) ==> I < Imax | S >= 0, S =< 2.
+cell(I,J), east(I,J,E), size(_,Jmax) ==> J < Jmax | E >= 0, E =< 2.
+cell(I,J), west(I,J,W) ==> J > 1 | W >= 0, W =< 2. 
 
 % Constraint 5
 sum(I,J, Sum), north(I,J,N), east(I,J,E), south(I,J,S), west(I,J,W) ==> Sum > 0 | sum(I,J, N + E + S + W).
 
 % Constraint 4
-sum(I,J,Sum), north(I,J,N) ==> Sum > 0 | N >= 0, N <= 2.
-sum(I,J,Sum), east(I,J,E) ==> Sum > 0 | E >= 0, E <= 2.
-sum(I,J,Sum), south(I,J,S) ==> Sum > 0 | S >= 0, S <= 2.
-sum(I,J,Sum), west(I,J,W) ==> Sum > 0 | W >= 0, W <= 2.
+sum(I,J,Sum), north(I,J,N) ==> Sum > 0 | N >= 0, N =< 2.
+sum(I,J,Sum), east(I,J,E) ==> Sum > 0 | E >= 0, E =< 2.
+sum(I,J,Sum), south(I,J,S) ==> Sum > 0 | S >= 0, S =< 2.
+sum(I,J,Sum), west(I,J,W) ==> Sum > 0 | W >= 0, W =< 2.
 
 % Constraint 3
 sum(I,J,Sum), north(I,J,N), south(I,J,S) ==> Sum == 0 | N == S.
