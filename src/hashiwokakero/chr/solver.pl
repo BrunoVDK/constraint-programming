ae68449caa11cc8376a359c7e1afc1b98e7d0382
Bruno Vandekerkhove <Buno@Brunos-MacBook-Air.local>
2019-05-29 00:36:59 +0200

    Debug + One additional constraint

--- src/hashiwokakero/chr/solver.pl
+++ src/hashiwokakero/chr/solver.pl
@@ -9,6 +9,12 @@
 %   5. sum constraint
 %   6. connectedness constraint
 %
+% They're all active. In the case of 1 abd 2 it is done by
+%   the sharing of variables between cells and islands.
+%   (e.g. any cell's 'north' variable is the same as the 'south' variable
+%       of the cell above it)
+% Constraint 4 is done by defining the domains to be 0..2
+%
 % @author   Michaël Dooreman & Bruno Vandekerkhove
 % @version  1.0
 
@@ -20,9 +26,13 @@
 :- chr_constraint print_board/1, print_cell/3, symbol/2.
 :- chr_constraint initialized/0, search/0, island/7, cell/4.
 :- chr_constraint in/2, assign/2.
+:- chr_constraint sink/2, no_sink/0.
 :- chr_constraint sum/3.
 
-% Sum constraint
+% Sum constraint (does forward checking)
+sum(4,[A,B,C,D],8) <=> A in [2], B in [2], C in [2], D in [2].
+sum(3,[A,B,C],6) <=> A in [2], B in [2], C in [2].
+sum(2,[A,B],4) <=> A in [2], B in [2].
 sum(1,[X],Val) <=> X in [Val].
 assign(Val,X) \ sum(N,Vars,Sum) # passive <=>
     N > 1, select(X, Vars, NewVars)
@@ -37,13 +47,18 @@ _ in [] <=> fail.
 X in L1, X in L2 <=> intersection(L1,L2,L3) | X in L3.
 X in L <=> nonvar(X) | member(X,L).
 
-% Search procedure (first-fail)
+% Search procedure
+%   Implementing first-fail is easy because domains are very small.
+%   We tried it, didn't improve speed.
 assign(Val,X) <=> X is Val.
 search, (X in Dom) # passive <=> member(Val,Dom), assign(Val,X), search.
 search <=> true.
 
+% Assign sink
+island(R,C,_,_,_,_,_) \ no_sink # passive <=> sink(R,C).
+
 % Print the solution
-% Assumes fixed-width font
+% Assumes fixed-width font (change in Settings > Font ...)
 print_board(Size) <=>
     print_cell(1,1,Size).
 print_cell(R,_,Size) <=> R > Size | true.
@@ -72,6 +87,7 @@ initialized <=> true.
 % Read the input puzzle
 solve_puzzle(P,Size,Verbose) <=>
     findall(0, between(1,Size,_), PrecedingSouths),
+    no_sink,
     read_puzzle(P,1,1,Size,PrecedingSouths,0),
     (Verbose -> print_board(Size) ; true).
 read_puzzle(_,R,_,Size,_,_) <=> R > Size | initialized, search.
@@ -112,7 +128,7 @@ solve(Identifier, Time, Verbose) :-
     puzzle(Identifier, N, Puzzle),
     (Verbose -> write('Puzzle name : '), write(Identifier), nl ; true),
     statistics(walltime, [_|[_]]),
-    solve_puzzle(Puzzle,N,Verbose), !,
+    once(solve_puzzle(Puzzle,N,Verbose)),
     statistics(walltime, [_|[Time]]),
     (Verbose -> write('Time : '), write(Time), nl ; true),
     (Verbose -> print_solution : true).
